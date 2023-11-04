75e4bb070c13227014a4d95f4dc9288305bbe425
Chris Vilches <felovilches@gmail.com>
2023-11-04 18:50:07 +0900

    attempt to fix the zombie process issue

--- src/server/global_context.rs
+++ src/server/global_context.rs
@@ -1,21 +1,29 @@
-use super::process_manager::ProcessManager;
+use std::sync::{Arc, Mutex};
+
+use super::process_manager::{start_garbage_collection, ProcessManager};
 use crate::{cmd::Cmd, sequence_automata::SequenceAutomata};
 
 pub struct MainProcessState {
   pub commands: Vec<Cmd>,
   pub pre_script: String,
   pub automata: SequenceAutomata,
-  pub process_manager: ProcessManager,
+  pub process_manager: Arc<Mutex<ProcessManager>>,
   pub interpreter: String,
 }
 
 impl MainProcessState {
   pub fn new(interpreter: &str) -> Self {
+    let process_manager = Arc::new(Mutex::new(ProcessManager::new()));
+    start_garbage_collection(
+      Arc::clone(&process_manager),
+      std::time::Duration::from_secs(2),
+    );
+
     Self {
       commands: vec![],
       pre_script: String::new(),
       automata: SequenceAutomata::new(&[]),
-      process_manager: ProcessManager::new(),
+      process_manager,
       interpreter: interpreter.to_owned(),
     }
   }
